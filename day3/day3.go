3fcfd4a464c557ef1a0f63fff21fdde90d04d7eb
Fabian Petersen <fabian@nf-petersen.de>
2024-12-09 16:17:20 +0100

    update day3

--- day3/day3.go
+++ day3/day3.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"aoc-lib/aoc"
+	"aoc-lib/its"
 	"aoc-lib/utils"
 	"fmt"
 	"io"
 	"regexp"
+	"slices"
 	"strconv"
 )
 
@@ -17,33 +19,34 @@ func (*Day3) Part1(r io.Reader) int {
 	matcher := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
 	data := string(utils.Must(io.ReadAll(r)))
 	matches := matcher.FindAllStringSubmatch(data, -1)
-	var sum int
-	for _, match := range matches {
-		l, r := utils.Must(strconv.Atoi(match[1])), utils.Must(strconv.Atoi(match[2]))
-		sum += l * r
-	}
-
-	return sum
+	return its.Reduce(slices.Values(matches), 0, func(acc int, v []string) int {
+		l, r := utils.Must(strconv.Atoi(v[1])), utils.Must(strconv.Atoi(v[2]))
+		return acc + l*r
+	})
 }
 
 func (*Day3) Part2(r io.Reader) int {
 	matcher := regexp.MustCompile(`(?:mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))`)
 	data := string(utils.Must(io.ReadAll(r)))
 	matches := matcher.FindAllStringSubmatch(data, -1)
-	var sum int
-	enabled := true
-	for _, match := range matches {
-		action := match[0]
-		if action == "do()" {
-			enabled = true
-		} else if action == "don't()" {
-			enabled = false
-		} else if enabled {
-			var l, r int
-			fmt.Sscanf(action, "mul(%d,%d)", &l, &r)
-			sum += l * r
-		}
+
+	type result struct {
+		enabled bool
+		sum     int
 	}
 
-	return sum
+	res := its.Reduce(slices.Values(matches), &result{enabled: true}, func(acc *result, v []string) *result {
+		action := v[0]
+		if action == "do()" {
+			acc.enabled = true
+		} else if action == "don't()" {
+			acc.enabled = false
+		} else if acc.enabled {
+			var l, r int
+			fmt.Sscanf(action, "mul(%d,%d)", &l, &r)
+			acc.sum += l * r
+		}
+		return acc
+	})
+	return res.sum
 }
